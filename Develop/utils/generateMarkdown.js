37b51ce9403d9c853290e660984bff796bc56442
taylorgehrts <taylorgehrts@gmail.com>
2023-06-29 17:36:31 -0700

    created functions to deal with licenses and made the general markdown structure, still need to work on the badges, not working like i had hoped

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,22 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function to render the license badge based on the license value
 function renderLicenseBadge(license) {
-  
+  return license ? `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})` : '';
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to render the license link based on the license value
+function renderLicenseLink(license) {
+  return license ? `[License](https://opensource.org/licenses/${license})` : '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render the license section based on the license value
+function renderLicenseSection(license) {
+  return license ? `## License\nThis project is licensed under the ${license} license. Click ${renderLicenseLink(license)} for more information.` : '';
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(answers) {
+  const licenseBadge = renderLicenseBadge(answers.License);
+
   return `# ${answers.Title}
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${answers.Description}
+
+## Installation
+${answers.Installation}
+
+## Usage
+${answers.Usage}
+
+## License
+${licenseBadge}
+${renderLicenseSection(answers.License)}
+
+## Contributing
+${answers.Contributing}
+
+## Tests
+${answers.Tests}
+
+## Questions
+If you have any questions, you can reach out to me via GitHub:
+
+- GitHub: [${answers.Questions}](https://github.com/${answers.Questions})
 `;
 }
+
+
 
 module.exports = generateMarkdown;
